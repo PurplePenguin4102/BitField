dce577de944b9fa0080482957abf8bf31fdc2d1b
Joey <joseph.h.ray@gmail.com>
2017-06-05 08:12:11 +1000

    boitfield

---
+++ BitFieldTests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitFieldTests
+{
+    [TestClass]
+    public class BitFieldTests
+    {
+        [TestMethod]
+        public void IntTest()
+        {
+        }
+    }
+}
