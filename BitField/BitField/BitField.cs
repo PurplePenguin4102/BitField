5d7e7a2fb6bdebbf43629391d597490e6d4c7117
Joseph Ray <joseph.h.ray@gmail.com>
2017-06-06 18:24:49 +1000

    recommit with links

---
+++ BitField/BitField/BitField.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitField
+{
+    public class BitField
+    {
+        private List<bool> _flags { get; set; }
+        public bool this[int key]
+        {
+            get
+            {
+                CheckRange(key);
+                return _flags[key];
+            }
+            set
+            {
+                CheckRange(key);
+                _flags[key] = value;
+            }
+        }
+
+        private bool CheckRange(int key)
+        {
+            if (key >= _flags.Count)
+                throw new IndexOutOfRangeException();
+            return true;
+        }
+
+        public BitField(int flags)
+        {
+            _flags = new List<bool>(32);
+            PopulateFlags(flags);
+        }
+
+        public BitField(short flags)
+        {
+            _flags = new List<bool>(16);
+            PopulateFlags(flags);
+        }
+
+        public BitField(byte flags)
+        {
+            _flags = new List<bool>(8);
+            PopulateFlags(flags);
+        }
+
+        private void PopulateFlags(int flags)
+        {
+            for (int i = 0; i < _flags.Count; i++)
+            {
+                _flags[i] = (flags & (1 << i)) > 0;
+            }
+        }
+    }
+}
---
+++ BitField/BitFieldTests/BitFieldTests.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitField;
+
+namespace BitFieldTests
+{
+    [TestClass]
+    public class BitFieldTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
